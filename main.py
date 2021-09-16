c2b4b036a57aee28bebb3141f4ba77ff463078e6
cyrus-git <kurimarron238@gmail.com>
2021-09-16 16:54:27 +0900

    create chart app

--- main.py
+++ main.py
@@ -1,26 +1,79 @@
-import time
-
+import altair as alt
+import pandas as pd
 import streamlit as st
+import yfinance as yf
 
-st.title('Streamlit Sample Apps')
+st.title('米国株価可視化アプリ')
 
-st.write('プログレスバーの表示')
-'Start!!'
+st.sidebar.write("""
+# GAFA株価
+こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
+""")
+st.sidebar.write("""
+## 表示日数選択
+""")
+days = st.sidebar.slider('日数', 1, 50, 20)
 
-latest_iteration = st.empty()
-bar = st.progress(0)
+st.write(f""""
+### 過去 **{days}日間** のGAFA株価
+""")
 
-for i in range(100):
-  latest_iteration.text(f'Iteration {i + 1}')
-  bar.progress(i + 1)
-  time.sleep(0.03)
+@st.cache
+def get_data(days, tickers):
+  df = pd.DataFrame()
+  for company in tickers.keys():
+    tkr = yf.Ticker(tickers[company])
+    hist = tkr.history(period=f'{days}d')
+    hist.index = hist.index.strftime('%d %B %Y')
+    hist = hist[['Close']]
+    hist.columns = [company]
+    hist = hist.T
+    hist.index.name = 'Name'
+    df = pd.concat([df, hist])
+  return df
 
-'Done!!!'
+try:
+  st.sidebar.write("""
+  ## 株価の範囲指定
+  """)
 
-left_column, right_column = st.columns(2)
-button = left_column.button('右カラムに文字を表示')
-if button:
-  right_column.write('ここは右カラムです')
+  ymin, ymax = st.sidebar.slider('範囲を指定してください。', 0.0, 4000.0, (0.0, 4000.0))
 
-expander = st.expander('問い合わせ')
-expander.write('問い合わせの回答')
+  tickers = {
+    'google': 'GOOGL',
+    'amazon': 'AMZN',
+    'facebook': 'FB',
+    'apple': 'AAPL',
+    'microsoft': 'MSFT',
+    'netflix': 'NFLX'
+  }
+
+  df = get_data(days, tickers)
+
+  companies = st.multiselect(
+    '会社名を選択してください。',
+    list(df.index),
+    ['google', 'amazon', 'facebook', 'apple']
+  )
+
+  if not companies:
+    st.error('少なくとも一社は選んでください。')
+  else:
+    data = df.loc[companies]
+    st.write('### 株価 (USD)', data.sort_index())
+    data = data.T.reset_index()
+    data = pd.melt(data, id_vars=['Date']).rename(columns={'value': 'Stock Prices(USD)'})
+    chart = (
+      alt.Chart(data)
+      .mark_line(opacity=0.8, clip=True)
+      .encode(
+        x='Date:T',
+        y=alt.Y('Stock Prices(USD):Q', stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+        color='Name:N'
+      )
+    )
+    st.altair_chart(chart, use_container_width=True)
+except:
+  st.error(
+    "エラーが発生しました。"
+  )
